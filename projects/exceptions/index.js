492f17442bdb66be48475863a04904ec759f9285
SvetoslavSO <sshaunin@mail.ru>
2022-05-05 21:09:43 +0300

    3-rd homework exc

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -17,11 +17,14 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-  if (typeof fn != 'function') throw new Error('fn is not a function');
-  else if (!Array.isArray(array) || array.length === 0) throw new Error('empty array');
 
-  for (let i = 0; i < array.length; i++) {
-    if (fn(array[i]) === false) {
+  if(typeof (fn) != 'function')
+    throw new Error("fn is not a function")
+  else if(!(Array.isArray(array)) || array.length === 0 )
+    throw new Error("empty array");
+
+  for (let i=0 ; i<array.length; i++){
+    if(fn(array[i])=== false){
       return false;
     }
   }
@@ -45,11 +48,13 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-  if (typeof fn != 'function') throw new Error('fn is not a function');
-  else if (!Array.isArray(array) || array.length === 0) throw new Error('empty array');
+  if(typeof (fn) != 'function')
+    throw new Error("fn is not a function")
+  else if(!(Array.isArray(array)) || array.length === 0 )
+    throw new Error("empty array");
 
-  for (let i = 0; i < array.length; i++) {
-    if (fn(array[i]) === true) {
+  for(let i = 0; i<array.length; i++){
+    if(fn(array[i]) === true){
       return true;
     }
   }
@@ -68,12 +73,13 @@ function isSomeTrue(array, fn) {
    - fn не является функцией (с текстом "fn is not a function")
  */
 function returnBadArguments(fn, ...args) {
-  if (typeof fn != 'function') throw new Error('fn is not a function');
-  const arr = [];
-  for (let i = 0; i < args.length; i++) {
-    try {
+  if(typeof (fn) != 'function')
+    throw new Error("fn is not a function")
+  var arr = [];
+  for(let i = 0; i<args.length; i++){
+    try{
       fn(args[i]);
-    } catch (e) {
+    }catch(e){
       arr.push(args[i]);
     }
   }
@@ -98,28 +104,29 @@ function returnBadArguments(fn, ...args) {
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
 function calculator(number = 0) {
-  if (!isFinite(number)) {
-    throw new Error('number is not a number');
+  if(!isFinite(number)){
+    throw new Error("number is not a number");
   }
-  const obj = {
-    sum: function (...args) {
+  var obj = {
+    sum : function (...args) {
       return args.reduce((previos, current) => previos + current, number);
     },
-    dif: function (...args) {
+    dif : function (...args) {
       return args.reduce((previos, current) => previos - current, number);
     },
-    div: function (...args) {
-      const division = (previos, current) => {
-        if (previos === 0 || current === 0) {
+    div : function (...args) {
+      let division = (previos, current) => {
+        if ( (previos === 0) || (current === 0) )  {
           throw new Error('division by 0');
         }
         return previos / current;
-      };
+      }
       return args.reduce(division, number);
     },
-    mul: function (...args) {
+    mul : function (...args) {
       return args.reduce((previos, current) => previos * current, number);
-    },
+    }
+
   };
   return obj;
 }
