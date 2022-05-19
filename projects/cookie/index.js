ca9d28ade901ca47877d2b58e98633632cc7667c
SvetoslavSO <sshaunin@mail.ru>
2022-05-19 18:55:14 +0300

    7 homework cookie 1-st reduction

--- projects/cookie/index.js
+++ projects/cookie/index.js
@@ -45,11 +45,66 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
+function compare(string, chunk) {
+  return string.toLowerCase().indexOf(chunk.toLowerCase()) > -1;
+}
+
+function createCookie(cookiesArr, compare, filterValue = '') {
+  for (const cookie in cookiesArr) {
+    if (filterValue === '') {
+      addCookie(cookie, cookiesArr);
+    } else {
+      if (compare(cookie, cookiesArr[cookie])) {
+        addCookie(cookie, cookiesArr);
+      }
+    }
+  }
+}
+
+function addCookie(cookie, cookiesArr) {
+  const row = listTable.insertRow(listTable.rows.length);
+  const cell1 = row.insertCell(0);
+  cell1.innerText = cookie;
+  const cell2 = row.insertCell(1);
+  cell2.innerText = cookiesArr[cookie];
+  const cell3 = row.insertCell(2);
+  const button = document.createElement('BUTTON');
+  button.classList.add('delete-button');
+  button.textContent = 'удалить';
+  cell3.appendChild(button);
+}
+
 filterNameInput.addEventListener('input', function () {});
 
 addButton.addEventListener('click', () => {
   document.cookie = `${addNameInput.value} = ${addValueInput.value}`;
   listTable.innerHTML = '';
+  if (filterNameInput === '') {
+    const cookie = cookieParse();
+    createCookie(cookie);
+  } else {
+    const cookie = cookieParse(filterNameInput.value);
+    createCookie(cookie, compare, filterNameInput.value);
+  }
 });
 
-listTable.addEventListener('click', (e) => {});
+function cookieParse(input = '') {
+  const cookies = document.cookie.split(';').reduce((prev, current) => {
+    const [name, value] = current.split('=');
+    if (input === '') {
+      prev[name] = value;
+    } else {
+      if (compare(name, input)) {
+        prev[name] = value;
+      }
+    }
+    return prev;
+  }, {});
+  return cookies;
+}
+
+listTable.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete-button')) {
+    listTable.removeChild(e.target.ParentNode);
+  }
+});
